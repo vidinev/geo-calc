9c713e8196e53776c2b26db60ce18b3dab56874c
Anton V <vidinev777@gmail.com>
2024-08-27 06:18:12 +0300

    Add menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'geo';
+  menuItems = [{
+    title: 'Газопроникність',
+    image: 'bg-gas',
+    id: 1
+  }, {
+    title: 'Капілярний тиск',
+    image: 'bg-press',
+    id: 2
+  }, {
+    title: 'Центрифугування',
+    image: 'bg-centrifuge',
+    id: 3
+  }, {
+    title: 'Питомий опір',
+    image: 'bg-resistant ',
+    id: 4
+  }, {
+    title: 'Пористість',
+    image: 'bg-pores',
+    id: 5
+  }, {
+    title: 'Об\'ємна вага',
+    image: 'bg-volume',
+    id: 6
+  }, {
+    title: 'Мінералогічна густина',
+    image: 'bg-density',
+    id: 7
+  }, {
+    title: 'Карбонатність',
+    image: 'bg-carbon',
+    id: 8
+  }, {
+    title: 'Нафтонасичення',
+    image: 'bg-oil',
+    id: 9
+  }, {
+    title: 'С Органічний',
+    image: 'bg-c_organic',
+    id: 10
+  }, {
+    title: 'Об.вага в парафіні',
+    image: 'bg-wax',
+    id: 11
+  }];
 }
